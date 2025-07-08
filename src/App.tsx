9d1f4fe570e99ef79ba3d5e98030cc04ec6c7d61
brunoverpet <bverpet46@gmail.com>
2025-07-08 15:33:10 +0200

    feat: Navbar interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import Navbar from "./Navbar.tsx";
+import {Button} from "./components/ui/button.tsx";
 
 export default function App()  {
 
   return (
       <>
-          <Navbar/>
+            <Navbar/>
+          <div className="m-10">
+              <div className="mt-16">
+                <h1 className="text-5xl font-semibold">Dashboard</h1>
+                <div className="flex justify-center space-x-8">
+                  <Button variant="outline" className="px-10 py-8 text-3xl">Mes événements créés</Button>
+                  <Button variant="outline" className="px-10 py-8 text-3xl">Événements enregistrés</Button>
+                </div>
+              </div>
+
+              <div className="mt-20">
+                  <div className="flex justify-between space-x-8">
+                    <h2 className="text-5xl font-semibold">Événements en cours</h2>
+                    <Button> Créer un événement</Button>
+                  </div>
+              </div>
+          </div>
       </>
   )
 }
