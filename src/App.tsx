180513cb8e2c28bd78c8a357b2baf6abcb4640fb
brunoverpet <bverpet46@gmail.com>
2025-07-08 16:48:18 +0200

    feat: add mobile interface

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,26 @@ export default function App()  {
   return (
       <>
             <Navbar/>
-          <div className="m-10">
+          <div className="m-4 md:m-10">
               <div className="mt-16">
-                <h1 className="text-5xl font-semibold">Dashboard</h1>
-                <div className="flex justify-center space-x-8">
-                  <Button variant="outline" className="px-10 py-8 text-3xl">Mes événements créés</Button>
-                  <Button variant="outline" className="px-10 py-8 text-3xl">Événements enregistrés</Button>
+                <h1 className="text-5xl font-semibold mb-4">Dashboard</h1>
+                <div className="md:flex justify-center space-x-8 space-y-4">
+                  <Button variant="outline" className="md:px-10 py-8 text-3xl">Mes événements créés</Button>
+                  <Button variant="outline" className="md:px-10 py-8 text-3xl">Événements enregistrés</Button>
                 </div>
               </div>
 
               <div className="mt-20">
                   <div className="flex justify-between space-x-8">
-                    <h2 className="text-5xl font-semibold">Événements en cours</h2>
-                    <Button> Créer un événement</Button>
+                    <h2 className="text-xl md:text-5xl font-semibold">Événements en cours</h2>
+                    <Button size="lg">Créer un événement</Button>
                   </div>
 
-                  <div className="mt-16">
+                  <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                      <EventCard />
+                      <EventCard />
+                      <EventCard />
+                      <EventCard />
                       <EventCard />
                   </div>
               </div>
--- src/EventCard.tsx
+++ src/EventCard.tsx
@@ -4,26 +4,25 @@ import {Button} from "./components/ui/button.tsx";
 export default function EventCard  () {
    return (
        <>
-           <div className="inline-grid items-center justify-center py-6 px-6 gap-4 rounded-md text-sm font-medium transition-all border bg-background shadow-xs">
+           <div className="inline-grid py-6 px-6 gap-4 rounded-md text-sm font-medium transition-all border bg-background shadow-xs">
                <div className="flex items-center space-x-4">
                    <CalendarDays color="orange" size={45} />
-                   <span className="text-lg font-semibold max-w-xs line-clamp-1">Nom de l'événement très très long qui doit être limité</span>
+                   <span className="text-lg font-semibold max-w-fit line-clamp-1">Nom de l'événement très très long qui doit être limité</span>
                </div>
                <div className="flex items-center space-x-4">
                    <MapPin color="purple" size={45} />
-                   <span className="text-lg font-semibold  max-w-xs line-clamp-2">Emplacement très long aussi qui doit revenir à la ligne</span>
+                   <span className="text-lg font-semibold  max-w-fit line-clamp-2">Emplacement très long aussi qui doit revenir à la ligne</span>
                </div>
                <div className="flex items-center space-x-4">
                    <Users color="green" size={45} />
-                   <span className="text-lg font-semibold max-w-xs break-words">Nombre de personnes maximums</span>
+                   <span className="text-lg font-semibold max-w-fit break-words">Nombre de personnes maximums</span>
                </div>
                <div className="flex items-center space-x-4">
-                <span className="text-lg font-semibold max-w-xs line-clamp-4">
+                <span className="text-lg font-semibold max-w-fit line-clamp-4">
                     Description : is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </span>
                </div>
-
-               <Button className="mt-5">Voir</Button>
+               <Button className="mt-5" size="lg">Voir l'événement</Button>
            </div>
 
        </>
